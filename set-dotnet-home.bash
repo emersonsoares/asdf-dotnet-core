c466e59733069d99f544cf831900bd2562ae0210
Tieu Philippe KHIM <khim.tieu.philippe@gmail.com>
2023-04-05 22:38:39 +0200

    fix(env): fix MSBuildSDKsPath

--- set-dotnet-home.bash
+++ set-dotnet-home.bash
@@ -1,9 +1,10 @@
 asdf_update_dotnet_home() {
   local dotnet_path
-  dotnet_path="$(asdf which dotnet 2> /dev/null  )"
+  dotnet_path="$(asdf which dotnet 2> /dev/null)"
   if [[ -n "${dotnet_path}" ]]; then
-    export DOTNET_ROOT
-    DOTNET_ROOT="$(dirname "$(realpath "${dotnet_path}")")"
+    export DOTNET_ROOT="$(dirname "$(realpath "${dotnet_path}")")"
+    DOTNET_VERSION="$(dotnet --version)"
+    export MSBuildSDKsPath="$DOTNET_ROOT/sdk/$DOTNET_VERSION/Sdks"
   fi
 }
 
--- set-dotnet-home.fish
+++ set-dotnet-home.fish
@@ -1,7 +1,9 @@
 function asdf_update_dotnet_home --on-event fish_prompt
   set --local dotnet_path (asdf which dotnet)
   if test -n "$dotnet_path"
+    set --local dotnet_version (dotnet --version)
     set --local full_path (realpath "$dotnet_path")
     set -gx DOTNET_ROOT (dirname "$full_path")
+    set -gx MSBuildSDKsPath (dirname "$full_path/sdk/$dotnet_version/Sdks")
   end
 end
